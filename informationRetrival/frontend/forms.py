2478bacfa2f72a9d3692ed38a2d5f439893c49e5
Noopur <noopijain@gmail.com>
2017-04-08 19:01:58 +0800

    fix spelling suggestion

--- informationRetrival/frontend/forms.py
+++ informationRetrival/frontend/forms.py
@@ -3,6 +3,7 @@ FIELD_CHOICES = [('title','Movie Title'),
                  ('overview', 'Movie Overview')]
 class SearchForm(forms.Form):
     search_field = forms.MultipleChoiceField(
+        required=True,
         widget=forms.CheckboxSelectMultiple,
         choices=FIELD_CHOICES,
         label="Search By:"
